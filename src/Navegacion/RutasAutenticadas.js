d46170a6244359b0938d643eebdb3db672dab272
hectoralarcon <hectoralarconguerra@gmail.com>
2020-12-24 16:38:32 -0500

    iniciando diseño

--- src/Navegacion/RutasAutenticadas.js
+++ src/Navegacion/RutasAutenticadas.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {NavigationContainer} from "@react-navigation/native";
-import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
-import {createDrawerNavigator} from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 //import {Icon} from "react-native-elements";
 
 import TiendaStack from "./TiendaStack";
@@ -14,32 +14,47 @@ const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
 const TabBar = () => {
-    return(
-        <Tab.Navigator>
-            <Tab.Screen
-             component = {TiendaStack}
-             name = "tienda"
-             options = {{title: "Tienda"}}
-            />
-            <Tab.Screen
-              component = {MiTienda}
-              name = "mitienda"
-              options = {{title:""}}
-            />
-             <Tab.Screen
-              component = {PerfilStack}
-              name = "cuenta"
-              options = {{title:"Cuenta"}}
-            />
-        </Tab.Navigator>
+  return (
+    <Tab.Navigator
+      initialRouteName="tienda"
+      tabBarOptions={{
+        inactiveTintColor: "#fff",
+        activeTintColor: "#fff",
+        style: {
+          borderTopLeftRadius: 60,
+          borderTopRightRadius: 60,
+          alignItems: "center",
+          backgroundColor: "#128C7E",
+          paddingBottom: 5,
+        },
+      }}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color }) => mostrarIcono(route, color),
+      })}
+    >
+      <Tab.Screen
+        component={TiendaStack}
+        name="tienda"
+        options={{ title: "Tienda" }}
+      />
+      <Tab.Screen
+        component={MiTienda}
+        name="mitienda"
+        options={{ title: "" }}
+      />
+      <Tab.Screen
+        component={PerfilStack}
+        name="cuenta"
+        options={{ title: "Cuenta" }}
+      />
+    </Tab.Navigator>
+  );
+};
 
-    )
-}
-
-export default function RutasAutenticadas(){
-    return(
-        <NavigationContainer>
-            <TabBar/>
-        </NavigationContainer>
-    )
+export default function RutasAutenticadas() {
+  return (
+    <NavigationContainer>
+      <TabBar />
+    </NavigationContainer>
+  );
 }
