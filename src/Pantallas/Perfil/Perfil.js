0c1f63d8ec52feae1631237bd8b22e349a2378e0
hectoralarcon <hectoralarconguerra@gmail.com>
2021-01-14 23:24:46 -0500

    Avatar

--- src/Pantallas/Perfil/Perfil.js
+++ src/Pantallas/Perfil/Perfil.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import { View, Text, StyleSheet, StatusBar } from "react-native";
+import { Icon, Avatar, Input } from "react-native-elements";
 
 export default function Perfil() {
   return (
     <View>
       <StatusBar backgroundColor="#128c7e" />
       <CabeceraBG />
+      <HeaderAvatar />
     </View>
   );
 }
@@ -21,6 +23,25 @@ function CabeceraBG() {
   );
 }
 
+function HeaderAvatar(props) {
+  const cambiarfoto = () => {
+    console.log("Hola");
+  };
+
+  return (
+    <View style={styles.avatarinline}>
+      <Avatar
+        source={require("../../../assets/avatar.jpg")}
+        style={styles.avatar}
+        size="large"
+        rounded
+        showAccessory={true}
+        onAccessoryPress={cambiarfoto}
+      />
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   bg: {
     width: "100%",
@@ -30,5 +51,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#128C7E",
     justifyContent: "center",
     alignItems: "center",
+  },
+  avatarinline: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: -70,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
   },
 });
