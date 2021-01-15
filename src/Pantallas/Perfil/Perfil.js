83b23d76291185b9d617ad1c16d52030fab6fda8
hectoralarcon <hectoralarconguerra@gmail.com>
2021-01-14 23:00:24 -0500

    Perfil cabecera

--- src/Pantallas/Perfil/Perfil.js
+++ src/Pantallas/Perfil/Perfil.js
@@ -1,10 +1,34 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
 
 export default function Perfil() {
   return (
     <View>
-      <Text>Perfil</Text>
+      <StatusBar backgroundColor="#128c7e" />
+      <CabeceraBG />
     </View>
   );
 }
+function CabeceraBG() {
+  return (
+    <View>
+      <View style={styles.bg}>
+        <Text style={{ color: "#fff", fontSize: 18, fontWeight: "bold" }}>
+          Nombre
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bg: {
+    width: "100%",
+    height: 200,
+    borderBottomLeftRadius: 200,
+    borderBottomRightRadius: 200,
+    backgroundColor: "#128C7E",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
